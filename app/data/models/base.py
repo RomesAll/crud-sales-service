1d42b095817cf4261780288429d209f1147b3e44
RomesAll <romesky845@gmail.com>
2026-06-17 19:20:49 +0300

    feat(data): добавить словарь соответвсий кирилическим-латинским буквам

--- app/data/models/base.py
+++ app/data/models/base.py
@@ -3,6 +3,40 @@ from sqlalchemy.orm import DeclarativeBase
 
 columns = list[str]
 
+mapping_dict_alph = {
+    'А' : 'A',
+    'Б' : 'B',
+    'В' : 'V',
+    'Г' : 'G',
+    'Д' : 'D',
+    'Е' : 'E',
+    'Ё' : 'E',
+    'Ж' : 'ZH',
+    'З' : 'Z',
+    'И' : 'I',
+    'Й' : 'I',
+    'К' : 'K',
+    'Л' : 'L',
+    'М' : 'M',
+    'Н' : 'N',
+    'О' : 'O',
+    'П' : 'P',
+    'Р' : 'R',
+    'С' : 'S',
+    'Т' : 'T',
+    'У' : 'U',
+    'Ф' : 'F',
+    'Х' : 'KH',
+    'Ц' : 'TS',
+    'Ч' : 'CH',
+    'Ш' : 'SH',
+    'Щ' : 'SHCH',
+    'Ы' : 'Y',
+    'Э' : 'E',
+    'Ю' : 'IU',
+    'Я' : 'IA',
+}
+
 class Base(DeclarativeBase):
     __abstract__ = True
     metadata = MetaData()
