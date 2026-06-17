d598894956e7470f66639f20f2b14c2553e8d5eb
RomesAll <romesky845@gmail.com>
2026-06-17 18:45:02 +0300

    feat(data): добавить базовый класс для ORM моделей
    - Переопределен метод repr для удобного отображения объекта в отладке
    - Определен метод to_dict для получения объекта в виде словаря

---
+++ app/data/models/base.py
@@ -0,0 +1,21 @@
+from sqlalchemy import MetaData, event
+from sqlalchemy.orm import DeclarativeBase
+
+columns = list[str]
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+    metadata = MetaData()
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__}>(id={getattr(self, "category_id", None)})'
+
+    def to_dict(self, exclude: columns | None = None):
+        result = {}
+        for column in self.__table__.columns.keys():
+            data = getattr(self, column)
+            if exclude and column in exclude:
+                continue
+            result.update({column: data})
+        return result
+
