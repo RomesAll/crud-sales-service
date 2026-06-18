3384bbeafd326b9f4050e84af7731362f324ad94
RomesAll <romesky845@gmail.com>
2026-06-18 11:42:54 +0300

    feat(data): добавить init файл для импорта объектов и хранения версии пакета

---
+++ app/data/models/__init__.py
@@ -0,0 +1,14 @@
+from .base import Base
+from .category import Category
+from .product import Product
+from .sale import Sale
+
+__version__ = "1.0.0"
+
+__all__ = [
+    "Base",
+    "Category",
+    "Product",
+    "Sale",
+]
+
