c4e9e2e88addef4bedaa6d8076f5de329422c6af
RomesAll <romesky845@gmail.com>
2026-06-18 11:45:11 +0300

    refactor(data): добавить проверку подключения к бд

---
+++ app/data/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine, Engine, text
+from sqlalchemy.orm import sessionmaker
+from app.config import config
+
+def health_check_connection(engine: Engine):
+    try:
+        with engine.connect() as conn:
+            conn.execute(text("SELECT 1"))
+            config.logging.app.info("Соединение с БД установлено!")
+            return True
+    except Exception as e:
+        config.logging.app.fatal(f"Ошибка соединения с БД: {e}")
+        return False
+
+engine = create_engine(
+    url=config.postgres.url,
+    echo=False,
+    pool_size=5,
+    max_overflow=10
+)
+session_maker = sessionmaker(bind=engine)
+health_check_connection(engine)
