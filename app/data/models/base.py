c3692eee0f628d8fe31f34a0b161e3e721a74839
RomesAll <romesky845@gmail.com>
2026-06-18 11:40:32 +0300

    refactor(data): переместить генерацию slug в файл event.py

--- app/data/models/base.py
+++ app/data/models/base.py
@@ -1,41 +1,7 @@
-from sqlalchemy import MetaData, event
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
 
 columns = list[str]
-
-mapping_dict_alph = {
-    'А' : 'A',
-    'Б' : 'B',
-    'В' : 'V',
-    'Г' : 'G',
-    'Д' : 'D',
-    'Е' : 'E',
-    'Ё' : 'E',
-    'Ж' : 'ZH',
-    'З' : 'Z',
-    'И' : 'I',
-    'Й' : 'I',
-    'К' : 'K',
-    'Л' : 'L',
-    'М' : 'M',
-    'Н' : 'N',
-    'О' : 'O',
-    'П' : 'P',
-    'Р' : 'R',
-    'С' : 'S',
-    'Т' : 'T',
-    'У' : 'U',
-    'Ф' : 'F',
-    'Х' : 'KH',
-    'Ц' : 'TS',
-    'Ч' : 'CH',
-    'Ш' : 'SH',
-    'Щ' : 'SHCH',
-    'Ы' : 'Y',
-    'Э' : 'E',
-    'Ю' : 'IU',
-    'Я' : 'IA',
-}
 
 class Base(DeclarativeBase):
     __abstract__ = True
@@ -52,20 +18,3 @@ class Base(DeclarativeBase):
                 continue
             result.update({column: data})
         return result
-
-def generate_slug(text: str) -> str:
-    text = text.upper()
-    for char in text:
-        if char.isalpha() and char in mapping_dict_alph:
-            text = text.replace(char, mapping_dict_alph[char])
-    return text.lower()
-
-def checking_column_exist(column: tuple):
-    name, target = column
-    return name in target.__table__.columns
-
-@event.listens_for(Base, 'before_insert')
-def generate_slug_before_insert(mapper, connection, target):
-    is_column_exist = all(map(checking_column_exist, [('slug', target), ('name', target)]))
-    if not is_column_exist:
-        target.slug = generate_slug(target.name)
