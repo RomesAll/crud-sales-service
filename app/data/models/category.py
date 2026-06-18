6ac3d64c7a10e1b0ad7c66df8a3a8f02165e1e5b
RomesAll <romesky845@gmail.com>
2026-06-18 13:43:07 +0300

    fix(data): исправить дублирования атрибута id
    - Удаленный атрибут из класса Category дублирует и переопределяет миксин IdMixin, в котором такой же атрибут

--- app/data/models/category.py
+++ app/data/models/category.py
@@ -5,10 +5,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class Category(IdMixin, SlugMixin, TimestampMixin, Base):
     __tablename__ = 'categories'
-    id: Mapped[int] = mapped_column(
-        primary_key=True,
-        autoincrement=True
-    )
     name: Mapped[str] = mapped_column(
         String(100),
         unique=True,
