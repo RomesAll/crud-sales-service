abeda4df56bab8a0a6a709c67da8b61a7dd6f043
RomesAll <romesky845@gmail.com>
2026-06-18 11:41:33 +0300

    feat(data): добавить orm модель с продуктами

---
+++ app/data/models/product.py
@@ -0,0 +1,27 @@
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import mapped_column, Mapped
+from .base import Base
+from app.data.models.mixins import IdMixin, SlugMixin, TimestampMixin
+
+class Product(IdMixin, SlugMixin, TimestampMixin, Base):
+    __tablename__ = 'products'
+    name: Mapped[str] = mapped_column(
+        String(200),
+    )
+    category_id: Mapped[int] = mapped_column(
+        ForeignKey('categories.id', ondelete='SET NULL')
+    )
+    price: Mapped[float] = mapped_column(
+        default=0.0,
+        index=True
+    )
+    stock_quantity: Mapped[int] = mapped_column(
+        default=0
+    )
+    sku: Mapped[str] = mapped_column(
+        String(50),
+        unique=True,
+    )
+    is_active: Mapped[bool] = mapped_column(
+        default=True
+    )
