82060a7e03da1721c3436c25a8f59a879115c0a0
RomesAll <romesky845@gmail.com>
2026-06-18 11:39:30 +0300

    feat(data): добавить миксины для частых полей orm моделей

---
+++ app/data/models/mixins.py
@@ -0,0 +1,36 @@
+from sqlalchemy import DateTime, func, UUID, Uuid
+from sqlalchemy.orm import Mapped, mapped_column
+import uuid, datetime
+
+class IdMixin:
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        autoincrement=True,
+        index=True
+    )
+
+class UUIDMixin:
+    uuid: Mapped[UUID] = mapped_column(
+        Uuid,
+        primary_key=True,
+        index=True,
+        default=uuid.uuid4,
+    )
+
+class TimestampMixin:
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=datetime.datetime.now(tz=datetime.timezone.utc),
+        server_default=func.now()
+    )
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=datetime.datetime.now(tz=datetime.timezone.utc),
+        server_default = func.now()
+    )
+
+class SlugMixin:
+    slug: Mapped[str] = mapped_column(
+        unique=True,
+        index=True
+    )
