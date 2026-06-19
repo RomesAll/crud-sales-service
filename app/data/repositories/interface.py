060492a2f32ceaa430c7c6970964ec21dee24825
RomesAll <romesky845@gmail.com>
2026-06-19 17:53:51 +0300

    feat(data): добавить интерфейс для repo

---
+++ app/data/repositories/interface.py
@@ -0,0 +1,30 @@
+import uuid
+from abc import ABC, abstractmethod
+from typing import Any
+from app.data.models import Base
+
+
+class Repository(ABC):
+    @abstractmethod
+    def get_all(self, limit: int = 10, offset: int = 0):
+        pass
+
+    @abstractmethod
+    def get_by_id(self, id: int | uuid.UUID):
+        pass
+
+    @abstractmethod
+    def get_by_uuid(self, uuid: uuid.UUID):
+        pass
+
+    @abstractmethod
+    def insert(self, orm_object: Base):
+        pass
+
+    @abstractmethod
+    def update(self, id: int | uuid.UUID, update_data: dict):
+        pass
+
+    @abstractmethod
+    def delete(self, id: int | uuid.UUID):
+        pass
